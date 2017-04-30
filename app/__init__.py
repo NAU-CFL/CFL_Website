ba9836ada56266155db310df8bd46058f9863f03
eneskemalergin <eneskemalergin@gmail.com>
2017-04-30 00:55:09 -0500

    404 and 500 error pages added

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
 
+# to determine the root path of the application
 app = Flask(__name__)
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, make_response, redirect
 from app import app
 
 # Route decorators to referance urls
@@ -36,10 +36,37 @@ def research():
     return render_template('research.html')
 
 # Blog page will be linked to jekyll-markdown blog
-# @app.route('/blog')
-# def blog():
-#     return render_template('blog.html')
+@app.route('/blog')
+def blog():
+    return redirect('http://eneskemalergin.github.io')
 
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+# Error Handlers
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+
+# Experimental routes:
+# @app.route('/user/<name>') # <name> is dynamic part
+# @app.route('/user/<int:int_id>')
+# def user(int_id):
+#     if int_id == 9999:
+#         name = 'Enes'
+#         return('<h1>Hello, %s!</h1>' % name)
+#     else:
+#         return("<h1>Hello, Stranger!</h1>")
+#
+# @app.route('/response_example')
+# def response_example():
+#     response = make_response('<h1>This document carries a cookie!</h1>')
+#     response.set_cookie('answer', '42')
+#     return response
